736059ce5fed657a5fe81045037c19daf781171a
MarcusJuniusBrutus <120407919+MarcusJuniusBrutus@users.noreply.github.com>
2025-03-21 14:58:51 -0400

    miscellaneous edits

--- Rakuro/src/connection.java
+++ Rakuro/src/connection.java
@@ -4,10 +4,10 @@ public class connection {
     public static void main(String[] args) throws SQLException {
         Connection db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Rakuro", "postgres", "Newdelhi.8194!");
         Statement st = db.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM Department");
+        ResultSet rs = st.executeQuery("SELECT * FROM Room");
         while (rs.next()) {
             System.out.print("Column 1 returned: ");
-            System.out.println(rs.getString(1));
+            System.out.print(rs.getString(1));
         }
     }
 }
