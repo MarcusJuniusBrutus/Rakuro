6693f281647e1901faaafc80354b32ae8ce97b1c
Roni <veronicanartatez@Ronis-MacBook-Air.local>
2025-04-04 16:54:09 -0400

    created createRoomDamagesReport() method

--- Rakuro/src/main/java/com/demo/RoomService.java
+++ Rakuro/src/main/java/com/demo/RoomService.java
@@ -1,8 +1,9 @@
-package main.java.com.demo;
+package com.demo;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import PACKAGE_NAME.com.demo.Room;
@@ -51,7 +52,7 @@ public class RoomService {
 
                 //create a room with information given
                 Room new_room = new Room(hotel_chain_name, hotel_humber, room_number,
-                                         capacity, view_type, is_extendable, price);
+                        capacity, view_type, is_extendable, price);
 
                 //add to list of rooms
                 rooms.add(new_room);
@@ -73,22 +74,21 @@ public class RoomService {
     }
 
 
-
     /**
      * Method to return all available rooms
      *
-     * @param country the country the customer wants to find available rooms in
-     * @param province the province the customer wants to find available rooms in
-     * @param city the city the customer wants to find available rooms in
-     * @param hotel_chain_name the country the customer wants to find available rooms in
-     * @param room_capacity the maximum room capacity the customer wishes their room to have
-     * @param upperbound_price the maximum price the customer is willing to pay
-     * @param rating rating of the hotel
+     * @param country               the country the customer wants to find available rooms in
+     * @param province              the province the customer wants to find available rooms in
+     * @param city                  the city the customer wants to find available rooms in
+     * @param hotel_chain_name      the country the customer wants to find available rooms in
+     * @param room_capacity         the maximum room capacity the customer wishes their room to have
+     * @param upperbound_price      the maximum price the customer is willing to pay
+     * @param rating                rating of the hotel
      * @param total_number_of_rooms the total number of rooms of that hotel
-     * @param start_date the date of the first day they would like to book their room
-     * @param start_time the time in which their booking will start
-     * @param end_date the date of the last day they would like to book their room
-     * @param end_time the time in which their booking will end
+     * @param start_date            the date of the first day they would like to book their room
+     * @param start_time            the time in which their booking will start
+     * @param end_date              the date of the last day they would like to book their room
+     * @param end_time              the time in which their booking will end
      * @return a list of all available rooms in the based on the parameters the provide database
      * @throws Exception when trying to connect to database
      */
@@ -98,16 +98,16 @@ public class RoomService {
                                         String start_time, String end_date, String end_time) throws Exception {
 
         String sql = "SELECT DISTINCT r.* FROM room r " +
-                     "NATURAL JOIN hotel h ON r.hotel_number = h.hotel_number AND r.hotel_chain_name = h.hotel_chain_name " +
-                     "WHERE h.country = ? AND h.province = ? AND h.city = ? " +
-                     "AND r.hotel_chain_name = ? AND r.capacity >= ? AND r.price <= ? " +
-                     "AND h.rating >= ? AND h.total_number_of_rooms >= ? " +
-                     "AND NOT EXISTS ( " +
-                     "  SELECT 1 FROM booking b " +
-                     "  WHERE b.hotel_chain_name = r.hotel_chain_name AND b.hotel_number = r.hotel_number AND b.room_number = r.room_number " +
-                     "  AND NOT (b.end_date < ? OR (b.end_date = ? AND b.end_time <= ?) " +
-                     "           OR b.start_date > ? OR (b.start_date = ? AND b.start_time >= ?))" +
-                     ")";
+                "NATURAL JOIN hotel h ON r.hotel_number = h.hotel_number AND r.hotel_chain_name = h.hotel_chain_name " +
+                "WHERE h.country = ? AND h.province = ? AND h.city = ? " +
+                "AND r.hotel_chain_name = ? AND r.capacity >= ? AND r.price <= ? " +
+                "AND h.rating >= ? AND h.total_number_of_rooms >= ? " +
+                "AND NOT EXISTS ( " +
+                "  SELECT 1 FROM booking b " +
+                "  WHERE b.hotel_chain_name = r.hotel_chain_name AND b.hotel_number = r.hotel_number AND b.room_number = r.room_number " +
+                "  AND NOT (b.end_date < ? OR (b.end_date = ? AND b.end_time <= ?) " +
+                "           OR b.start_date > ? OR (b.start_date = ? AND b.start_time >= ?))" +
+                ")";
 
         //connect to db
         ConnectionDB db = new ConnectionDB();
@@ -152,7 +152,7 @@ public class RoomService {
 
                 //create a room with information given
                 Room new_room = new Room(hotel_chain_name, hotel_humber, room_number,
-                                         capacity, view_type, is_extendable, price);
+                        capacity, view_type, is_extendable, price);
 
                 //add to list of rooms
                 available_rooms.add(new_room);
@@ -164,5 +164,35 @@ public class RoomService {
             db.close();
         }
         return available_rooms;
-     }
+    }
+
+    /**
+     * Adds a report of any problems or damages of a specific room and saves it in the db
+     *
+     * @param country the country the customer wants to find available rooms in
+     * @param province the province the customer wants to find available rooms in
+     * @param city the city the customer wants to find available rooms in
+     * @param hotel_chain_name the country the customer wants to find available rooms in
+     */
+
+    public void createRoomDamagesReport(String hotel_chain_name, String hotel_number, String room_number, String problem_damages) throws Exception{
+
+        String sql = "INSERT INTO Room_Problems_Damages (hotel_chain_name, hotel_number, room_number, problem_damages) " +
+                     "VALUES (?, ?, ?,?)";
+
+        ConnectionDB db = new ConnectionDB();
+
+        try (Connection con = db.getConnection()) {
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setString(1, hotel_chain_name);
+            stmt.setString(2, hotel_number);
+            stmt.setString(3, room_number);
+            stmt.setString(4, problem_damages);
+
+            stmt.executeUpdate();
+        }
+        db.close();
+    }
 }
+
