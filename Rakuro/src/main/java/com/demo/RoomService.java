23cd8b67807ff0772214070f12995bad3755dc65
MarcusJuniusBrutus <120407919+MarcusJuniusBrutus@users.noreply.github.com>
2025-04-04 20:29:52 -0400

    fixed roomservice

--- Rakuro/src/main/java/com/demo/RoomService.java
+++ Rakuro/src/main/java/com/demo/RoomService.java
@@ -13,68 +13,6 @@ import PACKAGE_NAME.com.demo.ConnectionDB;
 public class RoomService {
 
     /**
-     * Method to get all rooms from the database
-     *
-     * @return List of rooms from database
-     * @throws Exception when trying to connect to database
-     */
-
-    public List<Room> getRooms() throws Exception {
-
-        // sql query
-        String sql = "SELECT * FROM room";
-        // database connection object
-        ConnectionDB db = new ConnectionDB();
-
-        // data structure to keep all rooms retrieved from database
-        List<Room> rooms = new ArrayList<>();
-
-        //try to connect to database, catch any exceptions
-        try (Connection con = db.getConnection()) {
-            // prepare the statement
-            PreparedStatement stmt = con.prepareStatement(sql);
-
-            // get the results from executing the query
-            ResultSet rs = stmt.executeQuery();
-
-
-            // iterate through the result set
-            while (rs.next()) {
-
-                //get information of room i in rs
-                String hotel_chain_name = rs.getString("hotel_chain_name");
-                Integer hotel_humber = rs.getInt("hotel_number");
-                String room_number = rs.getString("room_number");
-                Integer capacity = rs.getInt("capacity");
-                String view_type = rs.getString("view_type");
-                boolean is_extendable = rs.getBoolean("is_extendable");
-                double price = rs.getDouble("price");
-
-                //create a room with information given
-                Room new_room = new Room(hotel_chain_name, hotel_humber, room_number,
-                        capacity, view_type, is_extendable, price);
-
-                //add to list of rooms
-                rooms.add(new_room);
-            }
-
-            //close the result set
-            rs.close();
-            // close the statement
-            stmt.close();
-            con.close();
-            db.close();
-
-            // return the rooms retrieved from database
-            return rooms;
-        } catch (Exception e) {
-            // throw any errors occurred
-            throw new Exception(e.getMessage());
-        }
-    }
-
-
-    /**
      * Method to return all available rooms
      *
      * @param country               the country the customer wants to find available rooms in
@@ -101,13 +39,12 @@ public class RoomService {
                 "NATURAL JOIN hotel h ON r.hotel_number = h.hotel_number AND r.hotel_chain_name = h.hotel_chain_name " +
                 "WHERE h.country = ? AND h.province = ? AND h.city = ? " +
                 "AND r.hotel_chain_name = ? AND r.capacity >= ? AND r.price <= ? " +
-                "AND h.rating >= ? AND h.total_number_of_rooms >= ? " +
-                "AND NOT EXISTS ( " +
-                "  SELECT 1 FROM booking b " +
-                "  WHERE b.hotel_chain_name = r.hotel_chain_name AND b.hotel_number = r.hotel_number AND b.room_number = r.room_number " +
-                "  AND NOT (b.end_date < ? OR (b.end_date = ? AND b.end_time <= ?) " +
-                "           OR b.start_date > ? OR (b.start_date = ? AND b.start_time >= ?))" +
-                ")";
+                "AND h.rating >= " + rating + " AND h.total_number_of_rooms >= " + total_number_of_rooms +
+                " AND NOT EXISTS ( " +
+                " SELECT 1 FROM booking b " +
+                " WHERE b.hotel_chain_name = r.hotel_chain_name AND b.hotel_number = r.hotel_number AND b.room_number = r.room_number " +
+                " AND NOT (b.end_date < '" + end_date + "' OR (b.end_date = '" + end_date + "' AND b.end_time <= '" + end_time + "') " +
+                " OR b.start_date > '" + start_date + "' OR (b.start_date = '" + start_date + "' AND b.start_time >= '" + start_time + "')))";
 
         //connect to db
         ConnectionDB db = new ConnectionDB();
@@ -125,17 +62,7 @@ public class RoomService {
             stmt.setString(4, hotel_chain_name);
             stmt.setString(5, room_capacity);
             stmt.setString(6, upperbound_price);
-            stmt.setString(7, rating);
-            stmt.setString(8, total_number_of_rooms);
-
-            //sets booking overlap filter parameters
-            //logic: excludes all bookings that do not end before we arrive or start after we leave
-            stmt.setString(9, start_date);
-            stmt.setString(10, start_date);
-            stmt.setString(11, start_time);
-            stmt.setString(12, end_date);
-            stmt.setString(13, end_date);
-            stmt.setString(14, end_time);
+            stmt.setString(7, total_number_of_rooms);
 
             ResultSet rs = stmt.executeQuery();
 
@@ -179,7 +106,7 @@ public class RoomService {
     public void createRoomDamagesReport(String hotel_chain_name, String hotel_number, String room_number, String problem_damages) throws Exception{
 
         String sql = "INSERT INTO Room_Problems_Damages (hotel_chain_name, hotel_number, room_number, problem_damages) " +
-                     "VALUES (?, ?, ?,?)";
+                     "VALUES (?, " hotel_number + ", ?,?)";
 
         ConnectionDB db = new ConnectionDB();
 
@@ -187,9 +114,8 @@ public class RoomService {
             PreparedStatement stmt = con.prepareStatement(sql);
 
             stmt.setString(1, hotel_chain_name);
-            stmt.setString(2, hotel_number);
-            stmt.setString(3, room_number);
-            stmt.setString(4, problem_damages);
+            stmt.setString(2, room_number);
+            stmt.setString(3, problem_damages);
 
             stmt.executeUpdate();
         }
